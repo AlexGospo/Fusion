8a3b103b1a058a5b66651b7f8bfc930a69ba2abe
AlexGospo <alexander.gospodinov2003@gmail.com>
2023-02-18 23:55:47 -0500

    testA4

--- fusion/settings.py
+++ fusion/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*%xt+yw=_n%1d@(f#1a8d@@dby(^2lc5%jyu*nu1kbrr!y2l1'
+#SECRET_KEY = 'django-insecure-_*%xt+yw=_n%1d@(f#1a8d@@dby(^2lc5%jyu*nu1kbrr!y2l1'
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django_*%xt+yw=_n%1d@(f#1a8d@@dby(^2lc5%jyu*nu1kbrr!y2l1')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fusion.up.railway.app', '127.0.0.1']
 
 
 # Application definition
@@ -140,3 +144,5 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://fusion.up.railway.app']
